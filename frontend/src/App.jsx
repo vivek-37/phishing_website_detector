9944f9fe40060179af015db0285ca8467c8a84a6
cvishnu007 <nagarajasudha.123@gmail.com>
2025-02-15 10:17:47 +0530

    Backend and Frontend of website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>FastAPI + React</h1>
+      <p>Message from backend: {message}</p>
+    </div>
+  );
+}
+
+export default App;
